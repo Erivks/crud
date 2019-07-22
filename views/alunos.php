d100fd52c57aab59351cf5663588e6872ce5d3eb
Erivks <mordecai.es27@gmail.com>
2019-07-22 16:12:26 -0300

    Organizando a tabela

--- views/alunos.php
+++ views/alunos.php
@@ -9,11 +9,16 @@
         <th>
             Data de nascimento
         </th>
+        <th>
+            Deletar
+        </th>
     </tr>
     <?php
         while($row = mysqli_fetch_array($queryAlunosResult)){
             echo '<tr><td>'.$row['nome'].'</td>';
-            echo '<td>'.$row['data_nascimento'].'</td></tr>';
+            echo '<td>'.$row['data_nascimento'].'</td>';
+            echo '<td><a href="../deletaAluno.php?id_aluno='
+                  . $row['id_aluno'] . '">Deletar</a></td></tr>';
         }
     ?>
 </table>
