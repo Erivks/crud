41a0d87e743e10efbb343589de92eeea173e3c3a
Erivks <mordecai.es27@gmail.com>
2019-07-22 11:42:58 -0300

    Inserindo placeholders

--- views/novaMatricula.php
+++ views/novaMatricula.php
@@ -3,6 +3,7 @@
 
 <form action="../processaMatricula.php" method="POST">
     <select name="selectAluno" id="selectAluno">
+        <option disable="disable">Selecione um aluno</option>
         <?php
             while($row = mysqli_fetch_array($queryAlunosResult)){
                 echo '<option value="'. $row['id_aluno'] .'">'. $row['nome'] .'</option>';
@@ -10,10 +11,12 @@
         ?>
     </select>
     <select name="selectCurso" id="selectCurso">
+        <option disable="disable">Selecione um curso</option>
         <?php
             while($row = mysqli_fetch_array($queryCursosResult)){
                 echo '<option value="'. $row['id_curso'] .'">'. $row['nome'] .'</option>';
             }
         ?>
     </select>
+    <input type="submit" value="Matricular aluno">
 </form>
