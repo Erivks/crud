2004ab8db44239877d211404ea9b49aa92557c4d
Erivks <mordecai.es27@gmail.com>
2019-07-22 15:53:23 -0300

    Mudando nome da variavel

--- deletaMatricula.php
+++ deletaMatricula.php
@@ -4,7 +4,7 @@ include 'db.php';
 
 $id_matricula = $_GET['id_matricula'];
 
-$queryDeletar = "DELETE FROM Alunos_Cursos WHERE id_aluno_curso = $id_matricula";
-mysqli_query($connection, $queryDeletar);
+$queryDeleteMatricula = "DELETE FROM Alunos_Cursos WHERE id_aluno_curso = $id_matricula";
+mysqli_query($connection, $queryDeleteMatricula);
 
 header('location:index.php?page=matriculas');
