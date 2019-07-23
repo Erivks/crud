1055f5d45034492258cfb0b2dc44d25e433e0543
Erivks <mordecai.es27@gmail.com>
2019-07-23 16:55:30 -0300

    Começo de tela de alteração de matricula

--- views/matriculas.php
+++ views/matriculas.php
@@ -10,6 +10,9 @@
             Curso
         </th>
         <th>
+            Editar
+        </th>
+        <th>
             Deletar
         </th>
     </tr>
@@ -17,6 +20,8 @@
         while($row = mysqli_fetch_array($queryAlunosCursosResult)){
             echo '<tr><td>'.$row['nome_aluno'].'</td>';
             echo '<td>'.$row['nome_curso'].'</td>';
+            echo '<td><a href="?page=novaMatricula&editar='
+                    .$row['id_aluno_curso'].'">Editar</a></td>';
             echo '<td><a href="../deletaMatricula.php?id_matricula='
                     .$row['id_aluno_curso'].'">Deletar</a></td><tr>';
         }
--- views/novaMatricula.php
+++ views/novaMatricula.php
@@ -1,22 +1,38 @@
-<h1>Inserir nova matricula</h1>
-<h2 id="titleAluno">Selecione o aluno</h2>
+<?php if(!isset($_GET['editar'])) { ?>
 
-<form action="../processaMatricula.php" method="POST">
-    <select name="selectAluno" id="selectAluno">
-        <option disable="disable">Selecione um aluno</option>
-        <?php
-            while($row = mysqli_fetch_array($queryAlunosResult)){
-                echo '<option value="'. $row['id_aluno'] .'">'. $row['nome'] .'</option>';
-            }
-        ?>
-    </select>
-    <select name="selectCurso" id="selectCurso">
-        <option disable="disable">Selecione um curso</option>
-        <?php
-            while($row = mysqli_fetch_array($queryCursosResult)){
-                echo '<option value="'. $row['id_curso'] .'">'. $row['nome'] .'</option>';
-            }
-        ?>
-    </select>
-    <input type="submit" value="Matricular aluno">
-</form>
+    <h1>Inserir nova matricula</h1>
+    <h2 id="titleAluno">Selecione o aluno</h2>
+
+    <form action="../processaMatricula.php" method="POST">
+        <select name="selectAluno" id="selectAluno">
+            <option disable="disable">Selecione um aluno</option>
+            <?php
+                while($row = mysqli_fetch_array($queryAlunosResult)){
+                    echo '<option value="'. $row['id_aluno'] .'">'. $row['nome'] .'</option>';
+                }
+            ?>
+        </select>
+        <select name="selectCurso" id="selectCurso">
+            <option disable="disable">Selecione um curso</option>
+            <?php
+                while($row = mysqli_fetch_array($queryCursosResult)){
+                    echo '<option value="'. $row['id_curso'] .'">'. $row['nome'] .'</option>';
+                }
+            ?>
+        </select>
+        <input type="submit" value="Matricular aluno">
+    </form>
+
+<?php } else { ?>
+    <?php $editarId = $_GET['editar']; ?>
+        <form action="../editaMatricula.php" method="POST">
+            <select name="selectAluno" id="selectAluno">
+                <?php 
+                    while($row = mysqli_fetch_array($queryAlunosCursosResult)){
+                        if($row['id_alunos_cursos'] != $editarId){
+                            echo '<option value="'.$row['id_aluno'].'">';
+                        }
+                    }
+                ?>
+            </select>
+        </form>
